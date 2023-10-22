80bdd1664b68ff801ba63905e46e8d7d92600072
himanshu094 <himanshushakya940@gmail.com>
2023-10-22 20:19:34 +0530

    21. improving the layout

--- RNproject/App.js
+++ RNproject/App.js
@@ -3,19 +3,43 @@ import { Button, StyleSheet, Text, TextInput, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.appContainer}>
-      <View>
-        <TextInput placeholder='Your course goal!' />
+
+      <View style={styles.inputContainer}>
+        <TextInput style={styles.textInput} placeholder='Your course goal!' />
         <Button title='Add Goal' />
       </View>
-      <View>
+
+      <View style={styles.goalsContainer}>
         <Text>List of goals...</Text>
       </View>
+
     </View>
   );
 }
 
 const styles = StyleSheet.create({
   appContainer: {
-    padding:50
+    flex:1,      //it will take 100% height
+    paddingTop:50,
+    paddingHorizontal:16
+  },
+  inputContainer:{
+    flex:1,       //it will take 20%  (1/5) height
+    flexDirection:'row',
+    justifyContent:'space-between',
+    gap:8,
+    alignItems:'center',
+    marginBottom:24,
+    borderBottomWidth:1,
+    borderColor:'#ccc'
+  },
+  textInput:{
+    borderWidth:1,
+    borderColor:'#ccc',
+    width:'70%',
+    padding:8
+  },
+  goalsContainer:{
+    flex:4
   }
 });
